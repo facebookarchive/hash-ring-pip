c4c90eb2ca33676149a7583d8a7e89e61c59ee09
Mike Krieger <mikeyk@instagram.com>
2012-01-31 18:37:28 -0800

    Renaming package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import subprocess
 subprocess.call(["git", 'submodule', 'init'])
 subprocess.call(["git", 'submodule', 'update'])
 
-setup (name = 'hash-ring',
+setup (name = 'chashring',
                 version = '1.0',
                 description = 'C implementation of ketama hashing',
                 package_dir = {'': 'hash-ring/lib/python'},
